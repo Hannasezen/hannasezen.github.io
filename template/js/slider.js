0dd8813e93ea5b8de747c4dc1fccd16b9847ab7c
Hanna Sezen <andreeva0649@gmail.com>
2019-02-22 19:04:15 +0200

    slider

--- template/js/slider.js
+++ template/js/slider.js
@@ -1,15 +1,46 @@
 //;(() => {
   let slides = document.querySelectorAll('.slider__img');
-  let points = document.querySelectorAll('.slider__point');
-  let slider = document.querySelector('#slider');
+  let slidesHolder = document.querySelector('#slider');
+  let points = [];
 
-  function removeClass(elem) {
-    elem.classList.remove('active');
+  (function renderPoints() {
+    let pointsHolder = doc.querySelector('#control-points');
+    for(let slide of slides) {
+      let point = document.createElement('li');
+      point.classList.add('slider__point');
+      points.push(point);
+      pointsHolder.appendChild(point);
+    }
+  })()
+
+  let activeSlide = slides[0];
+  let activePoint = points[0];
+
+  activeSlide.classList.add('active');
+  activePoint.classList.add('active');
+
+  function removeClass(items) {
+    for (let item of items) {
+      item.classList.remove('active');
+    }    
   }
+  // 0 i++ 1 i++ 2 
 
   function moveSlide() {
-
+    let index = (Array.from(slides)).indexOf(activeSlide);
+    removeClass(slides);
+    removeClass(points); 
+    index++;   
+    activeSlide = slides[index];
+    activeSlide.classList.add('active');
+    activePoint = points[index];
+    activePoint.classList.add('active');    
+    if (index === slides.length - 1) {
+      return activeSlide = slides[99];
+    }
   }
+
+  let interval = setInterval(moveSlide, 3000);
 
   
 
