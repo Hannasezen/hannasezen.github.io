0112be62f853d8003f9cf9e8e136f8bc791479fd
Hanna Sezen <andreeva0649@gmail.com>
2019-02-22 22:50:41 +0200

    filters

--- template/js/catalog.js
+++ template/js/catalog.js
@@ -4,3 +4,25 @@ doc.querySelector('#catalog-filters').addEventListener('click', function() {
   doc.querySelector('#filters').classList.toggle('open-filters');
 })
 
+// filters data
+let filters = doc.querySelectorAll('.filters__item');
+for (let filter of filters) {
+  filter.addEventListener('click', function(event) {
+    if (event.target.nodeName === 'LI') {
+      if(event.target.querySelectorAll('a')[0].classList.contains('not-selected')) {
+        filter.classList.remove('selected');
+      } else {
+        filter.classList.add('selected');
+        filter.querySelectorAll('.filter__subcategory')[0].innerHTML = event.target.querySelectorAll('a')[0].innerText; 
+      }           
+    } else if (event.target.nodeName === 'A') {
+      if(event.target.classList.contains('not-selected')) {
+        filter.classList.remove('selected');
+      } else {
+        filter.classList.add('selected');
+        filter.querySelectorAll('.filter__subcategory')[0].innerHTML = event.target.innerText;
+      } 
+    }
+  })
+}
+
