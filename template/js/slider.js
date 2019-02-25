ca6ec4cb4a489c4db488cf59a4161f598315fc5e
Hanna Sezen <andreeva0649@gmail.com>
2019-02-25 23:22:44 +0200

    no slider

--- template/js/slider.js
+++ template/js/slider.js
@@ -1,62 +1,28 @@
-//;(() => {
+;(() => {
   let slides = document.querySelectorAll('.slider__img');
-  let slidesHolder = document.querySelector('#slider');
   let points = [];
 
-  function renderPoints() {
+  (function renderPoints() {
     let pointsHolder = doc.querySelector('#control-points');
     for(let i = 0; i < slides.length; i++) {
       let point = document.createElement('li');
       point.classList.add('slider__point');
       points.push(point);
       pointsHolder.appendChild(point);
-
-      point.addEventListener('click', function(event) {
-        removeClass(slides);
-        removeClass(points); 
-        activePoint = point;
-        let i = points.indexOf(point);
-        activeSlide = slides[i];
-        activeSlide.classList.add('active');
-        activePoint.classList.add('active');
-        clearInterval(interval);
-        let interval = setInterval(moveSlide, 10000);
-      })
+      if (i === 0) {
+        point.classList.add('active');
+      }
     }
-  };
+  })()
 
-  window.onload = renderPoints();
-
-  let activeSlide = slides[0];
-  let activePoint = points[0];
-
-  activeSlide.classList.add('active');
-  activePoint.classList.add('active');
-
-  function interval() {
-    setInterval(moveSlide, 10000)
-  }
-
-  function removeClass(items) {
-    for (let i = 0; i < items.length; i++) {
-      items[i].classList.remove('active');
-    }    
-  }
-
+  let index = 0;
   function moveSlide() {
-    let index = (Array.from(slides)).indexOf(activeSlide);
-    removeClass(slides);
-    removeClass(points); 
-    index++;   
-    activeSlide = slides[index];
-    activeSlide.classList.add('active');
-    activePoint = points[index];
-    activePoint.classList.add('active');    
-    if (index === slides.length - 1) {
-      return activeSlide = slides[99];
-    }
+    slides[index].classList.remove('active');
+    points[index].classList.remove('active');
+    index = (index + 1) % slides.length;   
+    slides[index].classList.add('active');
+    points[index].classList.add('active');     
   }
 
   let interval = setInterval(moveSlide, 10000);
-
-//})()
+})()
