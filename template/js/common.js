322c3a06114db3e8d9479e9ca8f8f6107a530fc2
Hanna Sezen <andreeva0649@gmail.com>
2019-02-27 01:56:53 +0200

    refactoring renderCard function

--- template/js/common.js
+++ template/js/common.js
@@ -139,6 +139,37 @@ function lookLocalStorage () {
 
 window.onload = lookLocalStorage();
 
+function renderCard(item, cards) {
+  let card = doc.createElement('div');
+  card.classList.add('card');
+  let a = doc.createElement('a');
+  a.classList.add('card__img', 'start-card__img');
+  a.setAttribute('href', './item.html#' + item.title);
+  let img = doc.createElement('img');
+  img.setAttribute('src', item.img);
+  img.setAttribute('alt', "new arrivals photo");
+  let link = doc.createElement('div');
+  link.classList.add('card__link');
+  link.innerHTML = 'View item';
+  a.appendChild(img);
+  a.appendChild(link);
+  card.appendChild(a);
+
+  let text = doc.createElement('div');
+  text.classList.add('card__text');
+  let desc = doc.createElement('div');
+  desc.classList.add('card__desc');
+  desc.innerHTML = item.title;
+  let price = doc.createElement('div');
+  price.classList.add('card__price');
+  price.innerHTML = item.price.toFixed(2);
+  text.appendChild(desc);
+  text.appendChild(price);
+  card.appendChild(text);
+
+  cards.appendChild(card);
+}
+
 /*
   {
     title: 'Only Skinny Jeans',
--- template/js/start.js
+++ template/js/start.js
@@ -3,35 +3,7 @@ function renderItems () {
   let cards = doc.querySelector('#arrivals-cards');
   for (let i = 0; i < items.length; i++) {
     if (items[i].new === true) {
-
-        let card = doc.createElement('div');
-        card.classList.add('card');
-        let a = doc.createElement('a');
-        a.classList.add('card__img', 'start-card__img');
-        a.setAttribute('href', './item.html#' + items[i].title);
-        let img = doc.createElement('img');
-        img.setAttribute('src', items[i].img);
-        img.setAttribute('alt', "new arrivals photo");
-        let link = doc.createElement('div');
-        link.classList.add('card__link');
-        link.innerHTML = 'View item';
-        a.appendChild(img);
-        a.appendChild(link);
-        card.appendChild(a);
-
-        let text = doc.createElement('div');
-        text.classList.add('card__text');
-        let desc = doc.createElement('div');
-        desc.classList.add('card__desc');
-        desc.innerHTML = items[i].title;
-        let price = doc.createElement('div');
-        price.classList.add('card__price');
-        price.innerHTML = items[i].price.toFixed(2);
-        text.appendChild(desc);
-        text.appendChild(price);
-        card.appendChild(text);
-
-        cards.appendChild(card);
+      renderCard(items[i], cards);
       }
     } 
 }
