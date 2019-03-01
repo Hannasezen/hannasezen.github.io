75ebf181a95fb36ca9267813f7abde99ddc103be
Hanna Sezen <andreeva0649@gmail.com>
2019-03-01 21:31:51 +0200

    doc

--- template/js/banner.js
+++ template/js/banner.js
@@ -1,5 +1,5 @@
 // click on the banners image
-let banners = doc.querySelectorAll('.promos')[0];
+let banners = document.querySelectorAll('.promos')[0];
 banners.addEventListener('click', function(event) {
   if(event.target.classList.contains('promo__title') || event.target.classList.contains('promo__category')) {
     let link = event.target.closest('div').parentNode.querySelectorAll('a')[0].href;
