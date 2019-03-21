78b7c5b6a603862712fa7e0e9bc2a7eff89e6a8b
Hanna Sezen <andreeva0649@gmail.com>
2019-03-21 14:28:16 +0200

    babel & gitignore

---
+++ template/lib/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log(123);
+
+var arr = [1, 2, 3];
+var b = arr.filter(function (i) {
+  return i % 2 === 0;
+});
---
+++ template/src/index.js
@@ -0,0 +1,4 @@
+console.log(123)
+
+let arr = [1, 2, 3];
+let b = arr.filter(i => i % 2 === 0);
